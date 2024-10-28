3b34e7570872b0f3e5d4cafc730871060595cd1a
Jakub Szymsza <jakub@szymsza.cz>
2024-10-28 17:14:42 +0100

    Disable creating __pycache__ folder with dynamic imports

--- bci/web/blueprints/experiments.py
+++ bci/web/blueprints/experiments.py
@@ -143,6 +143,7 @@ def python_evaluation(project: str, experiment: str, directory: str):
     path = f"experiments/pages/{project}/{experiment}/{host}/{directory}/index.py"
 
     # Dynamically import the file
+    sys.dont_write_bytecode = True
     spec = importlib.util.spec_from_file_location(module_name, path)
     module = importlib.util.module_from_spec(spec)
     sys.modules[module_name] = module
