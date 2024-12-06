73d2656dfffcbcccc55cd2376efeb8619999488e
Gertjan <gertjan.franken@kuleuven.be>
2024-12-06 18:33:13 +0000

    Allow binary caching without external database

--- bci/configuration.py
+++ bci/configuration.py
@@ -70,16 +70,17 @@ class Global:
     def get_database_params() -> DatabaseParameters:
         required_database_params = ['BCI_MONGO_HOST', 'BCI_MONGO_USERNAME', 'BCI_MONGO_DATABASE', 'BCI_MONGO_PASSWORD']
         missing_database_params = [param for param in required_database_params if os.getenv(param) in ['', None]]
+        binary_cache_limit = int(os.getenv('BCI_BINARY_CACHE_LIMIT', 0))
         if missing_database_params:
             logger.info(f'Could not find database parameters {missing_database_params}, using database container...')
-            return container.run()
+            return container.run(binary_cache_limit)
         else:
             database_params = DatabaseParameters(
                 os.getenv('BCI_MONGO_HOST'),
                 os.getenv('BCI_MONGO_USERNAME'),
                 os.getenv('BCI_MONGO_PASSWORD'),
                 os.getenv('BCI_MONGO_DATABASE'),
-                int(os.getenv('BCI_BINARY_CACHE_LIMIT', 0)),
+                binary_cache_limit,
             )
             logger.info(f"Found database environment variables '{database_params}'")
             return database_params
