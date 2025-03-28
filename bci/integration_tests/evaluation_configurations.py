b7639187aafe816492fe64263bb23f9ff9debd68
Gertjan <gertjan.franken@kuleuven.be>
2025-03-28 18:15:16 +0000

    Improve integrations tests.
    One integration test now targets every single major release, making it
    easier to spot release specific issues.

--- bci/integration_tests/evaluation_configurations.py
+++ bci/integration_tests/evaluation_configurations.py
@@ -19,24 +19,24 @@ def get_default_evaluation_configuration() -> EvaluationConfiguration:
 def get_default_evaluation_range(mech_group: str, only_releases: bool) -> EvaluationRange:
     return EvaluationRange(
         mech_group,
-        (25, 130),
+        (20, 136),
         None,
         only_releases,
     )
 
-def get_default_sequence_config() -> SequenceConfiguration:
+def get_default_sequence_config(sequence_limit: int) -> SequenceConfiguration:
     return SequenceConfiguration(
         8,
-        50,
+        sequence_limit,
         'comp_search',
     )
 
-def get_default_evaluation_parameters(browser_name: str, mech_group: str, only_releases: bool) -> EvaluationParameters:
+def get_default_evaluation_parameters(browser_name: str, mech_group: str, sequence_limit: int=50, only_releases: bool=True) -> EvaluationParameters:
     return EvaluationParameters(
         get_default_browser_configuration(browser_name),
         get_default_evaluation_configuration(),
         get_default_evaluation_range(mech_group, only_releases),
-        get_default_sequence_config(),
+        get_default_sequence_config(sequence_limit),
         'integrationtests_' + browser_name
     )
 
@@ -44,6 +44,14 @@ def get_eval_parameters_list(mech_groups: list[str]) -> list[EvaluationParameter
     evaluation_parameters_list = []
     for browser_name in ['chromium', 'firefox']:
         for mech_group in mech_groups:
-            params = get_default_evaluation_parameters(browser_name, mech_group, True)
+            if mech_group == 'all_reproduced':
+                sequence_limit = 999
+            else:
+                sequence_limit = 50
+            params = get_default_evaluation_parameters(
+                browser_name,
+                mech_group,
+                sequence_limit=sequence_limit
+            )
             evaluation_parameters_list.append(params)
     return evaluation_parameters_list
