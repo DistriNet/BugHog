3655068354afc1401781706a7301ac3e14b44ac3
Gertjan <gertjan.franken@kuleuven.be>
2026-02-04 13:36:12 +0000

    Fix bug where API is called for v8_sandbox instead of v8

--- bughog/subject/js_engine/v8_sandbox/state_oracle.py
+++ bughog/subject/js_engine/v8_sandbox/state_oracle.py
@@ -2,6 +2,7 @@ import logging
 from typing import Literal
 
 from bughog.subject.js_engine.v8.state_oracle import V8StateOracle
+from bughog.version_control.conversion import bughog_service
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,12 @@ class V8SandboxStateOracle(V8StateOracle):
     def __init__(self, subject_type: str, subject_name: str) -> None:
         # There are no public executables, only artisanal.
         super().__init__(subject_type, subject_name, only_artisanal=True)
+
+    def get_most_recent_commit_nb(self) -> int:
+        """
+        We override this method because we want to call the API for v8, not v8_sandbox.
+        """
+        return bughog_service.find_latest_commit_info('v8').get('nb')
 
     def has_public_executable(self, state_index: int, state_type: Literal['release', 'commit']) -> bool:
         return False
